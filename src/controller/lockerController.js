1f72e1fda542d05f72ba8669792b182ec886c9b3
NikomiiC <queenandyria@hotmail.com>
2024-03-19 14:59:53 +0800

    complete locker crud

--- src/controller/lockerController.js
+++ src/controller/lockerController.js
@@ -1,14 +1,18 @@
 const mongoose = require("mongoose");
 const Locker = mongoose.model('Locker');
 const {sendError} = require('../util/constants');
-
+const serviceUtil = require("../controller/serviceController");
 
 /**
  * CONSTANTS
  */
 const OCCUPIED = 'Occupied';
 const VALID = 'Valid';
-
+const SMALL = 'Small';
+const MEDIUM = 'Medium';
+const LARGE = 'Large';
+const ENUM_STATUS = ['Occupied', 'Valid'];
+const ENUM_SIZE = ['Small', 'Medium', 'Large'];
 
 async function getOccupiedLockersByIds(locker_id_list) {
     try {
@@ -72,7 +76,7 @@ async function removeLocationByIds(locker_id_list) {
     try {
         return await Locker.updateMany(
             {_id: {$in: locker_id_list}},
-            {$unset: { location_id: ""}}
+            {$unset: {location_id: ""}}
         );
 
         // return await Locker.find(
@@ -84,11 +88,103 @@ async function removeLocationByIds(locker_id_list) {
     }
 }
 
+async function getLockerByTransactionId(trn_id) {
+    try {
+        return await Locker.find({trn_id: trn_id});
+    } catch (err) {
+        console.log(err.message);
+        sendError(err.message);
+    }
+}
+
+async function getLockerById(id) {
+    try {
+        return await Locker.findById(id);
+    } catch (err) {
+        console.log(err.message);
+        sendError(err.message);
+    }
+}
+
+async function updateStatus(locker_id, status) {
+    const locker = await getLockerById(locker_id);
+    let new_locker;
+    if (!locker) {
+        sendError('Update status fail, invalid feedback');
+    } else {
+        try {
+            new_locker = await Locker.findOneAndUpdate({_id: locker_id}, {
+                status: status,
+            }, {
+                returnOriginal: false
+            });
+            return new_locker;
+        } catch (err) {
+            sendError(err.message);
+        }
+    }
+}
+
+async function getAllLockers() {
+    try {
+        return await Locker.find();
+    } catch (err) {
+        console.log(err.message);
+        sendError(err.message);
+    }
+}
+
+async function getLockersByLocationId(location_id, status, size) {
+    /**
+     * default return order by valid first then occupied, size: s - m - l
+     * 1. have status no size
+     * 2. no status have size
+     * 3. have status have size
+     */
+
+    let m, add_status, add_size, s, result;
+
+    try {
+        m = {"$match": {$and: [{"status": {"$in": ENUM_STATUS}}, {"size": {"$in": ENUM_SIZE}}]}};
+        add_status = {"$addFields": {"__status_order": {"$indexOfArray": [ENUM_STATUS, "$status"]}}};
+        add_size = {"$addFields": {"__size_order": {"$indexOfArray": [ENUM_SIZE, "$size"]}}};
+
+        if ((serviceUtil.isStringValNullOrEmpty(status) && serviceUtil.isStringValNullOrEmpty(size)) || (!serviceUtil.isStringValNullOrEmpty(status) && serviceUtil.isStringValNullOrEmpty(size) && status === VALID)) {
+            s = {"$sort": {"__status_order": -1, "__size_order": 1}};
+        }
+        else if(!serviceUtil.isStringValNullOrEmpty(status) && serviceUtil.isStringValNullOrEmpty(size) && status === OCCUPIED){
+            m = {"$match": {$and: [{"status": status}, {"size": {"$in": ENUM_SIZE}}]}};
+            s = {"$sort": {"__size_order": 1}};
+        }
+        else if(serviceUtil.isStringValNullOrEmpty(status) && !serviceUtil.isStringValNullOrEmpty(size)){
+            s = {"$sort": {"__status_order": -1}};
+            m = {"$match": {$and: [{"status": {"$in": ENUM_STATUS}}, {"size": size}]}};
+        }
+        else{
+            m = {"$match": {$and: [{"status": status}, {"size": size}]}};
+            s = {"$sort": {"__status_order": -1, "__size_order": 1}};
+        }
+        result = await Locker.aggregate([m, add_status, add_size, s]);
+        console.log(result);
+
+        return result;
+    } catch (err) {
+        console.log(err.message);
+        sendError(err.message);
+    }
+}
+
+
 module.exports = {
     getOccupiedLockersByIds,
     deleteLockersByIds,
     insertManyLockers,
     getLocatedLockersByIds,
     updateLocationByIds,
-    removeLocationByIds
+    removeLocationByIds,
+    updateStatus,
+    getAllLockers,
+    getLockerById,
+    getLockersByLocationId,
+    getLockerByTransactionId
 }
--- src/routes/lockerRoutes.js
+++ src/routes/lockerRoutes.js
@@ -5,6 +5,9 @@ const {resResult, sendError} = require('../util/constants');
 const lockerController = require("../controller/lockerController");
 const userController = require("../controller/userController");
 const serviceUtil = require("../controller/serviceController");
+const feedbackController = require("../controller/feedbackController");
+const locationController = require("../controller/locationController");
+const transactionController = require("../controller/transactionController");
 const Locker = mongoose.model('Locker');
 const router = express.Router();
 router.use(requireAuth); // require user to sign in first
@@ -15,6 +18,48 @@ router.use(requireAuth); // require user to sign in first
 const USER = 'u';
 const ADMIN = 'admin';
 
+/**
+ * Method GET
+ */
+router.get('/all_lockers', async (req, res) => {
+    try {
+        const lockers = await lockerController.getAllLockers();
+        res.send(resResult(0, 'Successfully get all lockers', lockers));
+    } catch (err) {
+        return res.status(422).send(resResult(1, `Fail to get all lockers ` + err.message));
+    }
+});
+
+router.get('/locker/:id', async (req, res) => {
+    try {
+        const locker = await lockerController.getLockerById(req.params.id);
+        res.send(resResult(0, 'Successfully get locker', locker));
+    } catch (err) {
+        return res.status(422).send(resResult(1, `Fail to get locker ` + err.message));
+    }
+});
+
+router.get('/lockers/by_location_id/:id', async (req, res) => {
+    const location_id = req.params.id;
+    const status = req.query.status;
+    const size = req.query.size;
+    try {
+        const lockers = await lockerController.getLockersByLocationId(location_id, status, size);
+        res.send(resResult(0, 'Successfully get lockers', lockers));
+    } catch (err) {
+        return res.status(422).send(resResult(1, `Fail to get lockers ` + err.message));
+    }
+});
+
+router.get('/locker/by_trn_id/:id', async (req, res) => {
+    const trn_id = req.params.id;
+    try {
+        const locker = await lockerController.getLockerByTransactionId(trn_id);
+        res.send(resResult(0, 'Successfully get locker', locker));
+    } catch (err) {
+        return res.status(422).send(resResult(1, `Fail to get locker ` + err.message));
+    }
+});
 
 /**
  * Method POST
@@ -51,5 +96,75 @@ router.post('/create_lockers', async (req, res) => {
         return res.status(422).send(resResult(1, err.message));
     }
 });
+
+router.post('locker/update_status/:id', async (req, res) => {
+
+    const id = req.params.id;
+    const params = req.body;
+
+    try {
+        const role = await userController.getRole(req);
+        if (role === ADMIN) {
+            const new_locker = await lockerController.updateStatus(id, params.status);
+
+            res.send(resResult(0, `Successfully update status `, new_locker));
+        } else {
+            return res.status(422).send(resResult(1, "User has no permission to update location."));
+        }
+
+    } catch (err) {
+        return res.status(422).send(resResult(1, err.message));
+    }
+
+});
+
+router.post('locker/update_location/:id', async (req, res) => {
+
+    const id = req.params.id;
+    const params = req.body;
+
+    try {
+        const role = await userController.getRole(req);
+        if (role === ADMIN) {
+            await lockerController.updateLocationByIds(params.location_id, [id]);
+            const new_locker = lockerController.getLockerById(id);
+            res.send(resResult(0, `Successfully update location id `, new_locker));
+        } else {
+            return res.status(422).send(resResult(1, "User has no permission to update location."));
+        }
+    } catch (err) {
+        return res.status(422).send(resResult(1, err.message));
+    }
+
+});
+
+/**
+ * Method: DELETE
+ */
+router.delete('/delete_locker', async (req, res) => {
+    const params = req.body;
+    const locker_id_list = params.locker_list;
+// role check
+    try {
+        const role = await userController.getRole(req);
+        if (role === ADMIN) {
+            const occupiedLockers = await lockerController.getOccupiedLockersByIds(locker_id_list);
+            if (occupiedLockers.length === 0) {
+                //update transaction locker id to removed, try first not sure if can assign string to _id
+                await transactionController.updateRemovedLockersIdToNull(location.locker_list);
+                await lockerController.deleteLockersByIds(location.locker_list);
+            } else {
+                return res.status(422).send(resResult(1, `Failed to delete, lockers in use`));
+            }
+            res.send(resResult(0, `Successfully delete lockers ${locker_id_list}`));
+        } else {
+            return res.status(422).send(resResult(1, "User has no permission to update location."));
+        }
+
+    } catch (err) {
+        return res.status(422).send(resResult(1, err.message));
+    }
+});
+
 
 module.exports = router;
